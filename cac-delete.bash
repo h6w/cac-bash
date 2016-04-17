6e200c9f0fa5774b02b21b24cf5d7edd0d6d5e9a
SchroSct <clls760@gmail.com>
2016-04-17 06:17:45 -0700

    Uses cac-list to populate a list to delete servers
    Use at your own risk, but it's been working for me, and obviously you can potentially destroy a lot.

---
+++ cac-delete.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+url="https://panel.cloudatcost.com/api/v1/"
+select server in $(cac-list.bash | grep -v listservers | sed -e 's/"sid":"//g' -e 's/".*label":"/-name:/g' -e 's/".*//g')
+do
+echo "You chose $server"
+break
+done
+uri="cloudpro/delete.php"
+sid="$(echo ${server%-*})"
+key="APIKEYFROMTHEWEBSITEGOESHERE"
+login="YOUREMAIL@ADDRESS.COMGOESHERE"
+curl -s -k --data "key=${key}&login=${login}&sid=${sid}" "${url}${uri}"
