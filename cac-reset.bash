f8b9a225fa87fc1e468ca1bd83762478e2fc6ef3
SchroSct <clls760@gmail.com>
2016-04-28 19:01:32 -0700

    cac-reset

---
+++ cac-reset.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+key=""
+login=""
+url="https://panel.cloudatcost.com/api/v1/"
+uri="powerop.php"
+sid=""
+ansuser=""
+domain=""
+while true
+do
+   if ansible "$domain" -m ping -u "$ansuser" &>/dev/null
+   then
+      echo "$domain looks up from here, doing nothing."
+      sleep 3600
+   else
+      echo "$domain appears to be down, trying to reset."
+      if curl -s -o /dev/null -k --data "key=${key}&login=${login}&sid=${sid}&action=reset" "${url}${uri}"
+      then
+         echo "Reset appears to have worked, waiting 5 minutes."
+         sleep 300
+      else
+         echo "All is fucked."
+      fi
+   fi
+done
